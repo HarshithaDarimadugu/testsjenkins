3004eebc982afd1232c94c13e1bdbe9d6a912243
HarshithaDarimadugu <harshithadarimadugu4@gmail.com>
2024-08-17 21:56:20 +0530

    added report

--- jenkins_remote/src/test/java/tests/test01.java
+++ jenkins_remote/src/test/java/tests/test01.java
@@ -2,13 +2,29 @@ package tests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.File;
+
 import org.junit.jupiter.api.Test;
+
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
 
 class test01 {
 
 	@Test
 	void test1() {
+		String path = "C:\\Users\\DELL\\OneDrive\\Desktop\\selenium\\jenkins_remote\\reports\\jenkinsreport.html";
+		File file = new File(path);
+		ExtentReports report = new ExtentReports(path);
+		ExtentTest test = report.startTest("test_jenkins");
+		test.log(LogStatus.INFO, "execution started");
 		System.out.println("jenkins is executing");
+		test.log(LogStatus.PASS, "output executed");
+		test.log(LogStatus.INFO, "execution ended");
+		report.endTest(test);
+		report.flush();
+		report.close();
 	}
 	@Test
 	void test2() {
