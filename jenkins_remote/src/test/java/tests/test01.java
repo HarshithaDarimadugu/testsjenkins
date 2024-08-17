079b3d540271b445b8de336e80808cda4fe96eab
HarshithaDarimadugu <harshithadarimadugu4@gmail.com>
2024-08-17 21:41:18 +0530

    new file is pushing into git for remote execution to jenkins

---
+++ jenkins_remote/src/test/java/tests/test01.java
@@ -0,0 +1,18 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class test01 {
+
+	@Test
+	void test1() {
+		System.out.println("jenkins is executing");
+	}
+	@Test
+	void test2() {
+		System.out.println("jenkins - a CI/CD tool");
+	}
+
+}
---
+++ jenkins_remote/src/test/java/tests/test02.java
@@ -0,0 +1,18 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class test02 {
+
+	@Test
+	void test3() {
+		System.out.println("code is pushing from github");
+	}
+	@Test
+	void test4() {
+		System.out.println("git is version control system");
+	}
+
+}
